1a65c8969998d6e9919a6905ebaca50fccdcfab2
Eduardo Bohrer <nbluisrs@gmail.com>
2012-07-19 11:54:47 -0300

    Criado a classe de tweet com testes

--- assets/js/test/spec/TwitterSpec.js
+++ assets/js/test/spec/TwitterSpec.js
@@ -3,6 +3,7 @@ describe("Testing twitter timeline with jQuery", function() {
   
   beforeEach(function() {
     timeline = new Timeline();
+    tweet = new Tweet();
   });
   
   it("Should have a timeline", function() {
@@ -10,10 +11,29 @@ describe("Testing twitter timeline with jQuery", function() {
   });
   
   it("Should add new tweet to timeline", function() {
-    timeline.tweet('Luiza foi pro canadá.');
-    
-    expect(timeline.tweets.length).toBe(1);
+    timeline.tweet('Luiza foi pro canada.');    
+    expect(timeline.getTweets().length).toBe(1);
+    var firstTweet = $(timeline.getTweets()[0]);
+    expect(firstTweet.find('.text').html()).toBe('Luiza foi pro canada.');
   });
-      
+  
+  describe('Testing new tweet',function(){
+    var tweet;
+    beforeEach(function() {
+      tweet = new Tweet();
+    });
+    
+    it('should create a new instance of tweet',function(){
+       expect(tweet).not.toBe(null);
+    });
+    
+    it('should create a new tweet with text',function(){
+       tweet = new Tweet('Luiza foi pro canada.');
+       expect(tweet.getText()).toBe('Luiza foi pro canada.');
+       tweet = new Tweet('Luiza voltou do canada.');
+       expect(tweet.getText()).toBe('Luiza voltou do canada.');
+    });
+    
+  });
   
 });
